577c238fe3ed5a352421dda4b7fd3d08182e7114
Ryan Carbotte <ryancarbotte@gmail.com>
2020-12-05 10:56:03 -0600

    Set target resolution & grid size

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ int main(int argc, char *argv[])
 {
 	constexpr std::size_t kFramesPerSecond{60};
 	constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
-	constexpr std::size_t kScreenWidth{640};
-	constexpr std::size_t kScreenHeight{480};
-	constexpr std::size_t kGridWidth{32};
-	constexpr std::size_t kGridHeight{32};
+	constexpr std::size_t kScreenWidth{800};
+	constexpr std::size_t kScreenHeight{720};
+	constexpr std::size_t kGridWidth{10};
+	constexpr std::size_t kGridHeight{9};
 
 	Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
 	// TODO: Instantiate the Controller
