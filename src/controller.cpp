e38f68a707e29ed5759bb7d16b57015b2434098d
Ryan Carbotte <ryancarbotte@gmail.com>
2020-12-05 19:27:24 -0600

    Add optional WASD control scheme

--- src/controller.cpp
+++ src/controller.cpp
@@ -25,18 +25,23 @@ void Controller::HandleInput(bool &running, Player &player, Game &game) const
         {
             switch (e.key.keysym.sym)
             {
+                case SDLK_w:
                 case SDLK_UP:
                     ChangeDirection(player, Player::Direction::kUp);
                     break;
+                case SDLK_s:
                 case SDLK_DOWN:
                     ChangeDirection(player, Player::Direction::kDown);
                     break;
+                case SDLK_a:
                 case SDLK_LEFT:
                     ChangeDirection(player, Player::Direction::kLeft);
                     break;
+                case SDLK_d:
                 case SDLK_RIGHT:
                     ChangeDirection(player, Player::Direction::kRight);
                     break;
+                case SDLK_e:
                 case SDLK_SPACE:
                     Activate(player, game);
                     break;
